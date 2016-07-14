0c8799f364ba5c309b4b6c81411dc63c9eea1cb4
Zan_Huang <www.zandhuang@gmail.com>
2016-07-14 12:57:30 -0400

    New File

--- Process.py
+++ Process.py
@@ -6,7 +6,7 @@ import Input
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('batch_size', 100, "hello")
+tf.app.flags.DEFINE_integer('batch_size', 20, "hello")
 tf.app.flags.DEFINE_string('data_dir', '/Volumes/Machine_Learning_Data', "hello")
 
 def inputs():
@@ -28,43 +28,44 @@ def conv2d(images, W):
     return tf.nn.conv2d(images, W, strides = [1, 1, 1, 1], padding = 'SAME')
 
 def max_pool_5x5(images):
-    return tf.nn.max_pool(images, ksize = [1, 5, 5, 1], strides = [1, 1, 1, 1], padding = 'SAME')
+    return tf.nn.max_pool(images, ksize = [1, 5, 5, 1], strides = [1, 1, 1, 1], padding = 'VALID')
 
 def forward_propagation(images):
   with tf.variable_scope('conv1') as scope:
-      W_conv1 = weight_variable([5, 5, 1, 50])
-      b_conv1 = bias_variable([50])
-      image_matrix = tf.reshape(images, [-1, 1750, 1750, 1])
+      W_conv1 = weight_variable([5, 5, 3, 32])
+      b_conv1 = bias_variable([32])
+      image_matrix = tf.reshape(images, [-1, 1750, 1750, 3])
       h_conv1 = tf.nn.sigmoid(conv2d(image_matrix, W_conv1) + b_conv1)
       h_pool1 = max_pool_5x5(h_conv1)
 
   with tf.variable_scope('conv2') as scope:
-      W_conv2 = weight_variable([5, 5, 50, 100])
-      b_conv2 = bias_variable([100])
+      W_conv2 = weight_variable([5, 5, 32, 64])
+      b_conv2 = bias_variable([64])
       h_conv2 = tf.nn.sigmoid(conv2d(h_pool1, W_conv2) + b_conv2)
       h_pool2 = max_pool_5x5(h_conv2)
 
   with tf.variable_scope('conv3') as scope:
-      W_conv3 = weight_variable([5, 5, 100, 120])
-      b_conv3 = bias_variable([120])
+      W_conv3 = weight_variable([5, 5, 64, 128])
+      b_conv3 = bias_variable([128])
       h_conv3 = tf.nn.sigmoid(conv2d(h_pool2, W_conv3) + b_conv3)
       h_pool3 = max_pool_5x5(h_conv3)
 
   with tf.variable_scope('local3') as scope:
-      W_fc1 = weight_variable([100 * 100 * 120, 140])
-      b_fc1 = bias_variable([140])
-      h_pool3_flat = tf.reshape(h_pool3, [-1, 100 * 100 * 120])
+      W_fc1 = weight_variable([14 * 14 * 128, 256])
+      b_fc1 = bias_variable([256])
+      h_pool3_flat = tf.reshape(h_pool3, [FLAGS.batch_size, 14 * 14 * 128])
       h_fc1 = tf.nn.sigmoid(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
       keep_prob = tf.placeholder(tf.float32)
       h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-      W_fc2 = weight_variable([140, 4])
+      W_fc2 = weight_variable([256, 4])
       b_fc2 = bias_variable([4])
 
       y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
       return y_conv
 
 def error(forward_propagation_results, labels):
-    labels = tf.cast(labels, tf.float32)
+    labels = tf.one_hot([1, 2, 3, 4], 20, on_value=None, off_value=None, axis=None, dtype=None, name=None)
+    labels = tf.cast(labels, tf.int64)
     mean_squared_error = tf.square(tf.sub(labels, forward_propagation_results))
     cost = tf.reduce_mean(mean_squared_error)
     train = tf.train.GradientDescentOptimizer(learning_rate = 0.3).minimize(cost)
