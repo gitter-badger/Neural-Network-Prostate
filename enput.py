ab334feb7509c741ba68a493e6e4a1c16c9c76a7
StructML <struct.machine.learning@gmail.com>
2016-09-05 19:35:47 -0400

    Add files via upload

--- enput.py
+++ enput.py
@@ -1,21 +1,14 @@
 from PIL import Image
 import numpy as np
-import glob
-
+import glob, os
 
 for img in glob.glob("*.jpg"):
-	for i in range(1):
-		im = Image.open(img)
-		im = (np.array(im))
-		print(im)
-
-		r = im[:,:,0].flatten()
-		g = im[:,:,1].flatten()
-		b = im[:,:,2].flatten()
-		label = [0]
-
-		begin = np.array(list(label) + list(r) + list(g) + list(b), np.uint8)
-
-	new_array = np.array(list(r) + list(g) + list(b), np.uint8)
-	NEW = np.append(begin, new_array, 0)
-	NEW.tofile("Prostate_Cacer_Data1.bin")
+	with open("Prostate_Cancer_Data1", "wb") as f:
+	  	for img in glob.glob("*.jpg"):
+			label = np.array([0], dtype = np.uint8)
+			f.write(label.tostring())
+			im = np.array(Image.open(img), dtype = np.uint8)
+			f.write(im[:, :, 0].tostring())
+			f.write(im[:, :, 1].tostring())
+			f.write(im[:, :, 2].tostring())
+			print(im)
